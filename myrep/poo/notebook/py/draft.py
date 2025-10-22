9d274b30644a320f22677cb943e648954766a00b
cdzra <cadus824@gmail.com>
2025-10-22 14:34:12 +0000

    fofin

--- myrep/poo/notebook/py/draft.py
+++ myrep/poo/notebook/py/draft.py
@@ -8,8 +8,11 @@ class Carregador:
         self.__potencia: int = potencia
 
 class Notebook:
-    def __init__(self):
+    def __init__(self, minutos : int = 0 ):
         self.__ligado: bool = False
+        self.__bateria: Bateria  | None = None
+        self.__carregador: Carregador | None = None
+        self.minutos = minutos
 
     def getLigado(self):
         if self.__ligado == False:
@@ -31,6 +34,14 @@ class Notebook:
             return
         self.__ligado = False
 
+    def usar(self, minutos: int):
+        if self.__ligado == False:
+            print("fail: notebook desligado")
+            return
+        else:
+            self.minutos = minutos
+            return
+        
 def main():
     notebook = Notebook()
     while True:
@@ -46,4 +57,6 @@ def main():
             notebook.setLigar()
         elif args[0] == "desligar":
             notebook.setDesligar()
+        elif args[0] == "use":
+            print(f"Usado por {notebook.usar(args[1])} minutos")
 main()
